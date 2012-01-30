c5507d81f7a40adf0dfacb9b96ba96a4915357be
c9s <cornelius.howl@gmail.com>
2012-01-30 16:44:41 +0800

    Improve sqlbuilder expression

--- tests/SQLBuilder/ExpressionTest.php
+++ tests/SQLBuilder/ExpressionTest.php
@@ -3,7 +3,7 @@
 class ExpressionTest extends PHPUnit_Framework_TestCase
 {
 
-    function createExpr()
+    public function createExpr()
     {
         $builder = new SQLBuilder\CRUDBuilder('Foo');
         $builder->driver = new SQLBuilder\Driver;
@@ -12,20 +12,39 @@ class ExpressionTest extends PHPUnit_Framework_TestCase
         return $expr;
     }
 
-    function test()
+    public function testOp()
     {
         $expr = $this->createExpr();
         $expr->is( 'a' , 'null' )->is( 'b' , 'null' );
         is( 'a is null AND b is null', $expr->inflate() );
-
     }
 
-    function test2()
+    public function testOp2()
     {
         $expr = $this->createExpr();
         $expr->is( 'a' , 'null' )
             ->and()->is( 'b', 'null' );
         is( 'a is null AND b is null', $expr->inflate() );
     }
+
+
+    public function testOp3()
+    {
+        $expr = $this->createExpr();
+        $expr->is( 'a' , 'null' )
+            ->or()->is( 'b', 'null' );
+        is( 'a is null OR b is null', $expr->inflate() );
+    }
+
+    public function testOp4()
+    {
+        $expr = $this->createExpr();
+        $expr->isEqual( 'a' , 'foo' )
+            ->or()->isEqual( 'b', 'bar' );
+        is( "a = 'foo' OR b = 'bar'", $expr->inflate() );
+    }
+
+
+
 }
 
