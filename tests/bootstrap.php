cfeae0ba4b42d6ca52ea62cbd4a96293b0cdeb7f
c9s <cornelius.howl@gmail.com>
2012-01-31 13:42:34 +0800

    Use include

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,4 +2,5 @@
 require 'vendor/pear/PHPUnit_TestMore.php';
 require 'vendor/pear/Universal/ClassLoader/BasePathClassLoader.php';
 $loader = new \Universal\ClassLoader\BasePathClassLoader(array('src','vendor/pear'));
+$loader->useIncludePath(true);
 $loader->register();
