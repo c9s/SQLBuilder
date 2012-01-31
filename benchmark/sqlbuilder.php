6db2321e054999510a42149ab6649d13206df203
Yo-An Lin <yoanlin93@gmail.com>
2012-01-31 13:42:34 +0800

    Use include

--- benchmark/sqlbuilder.php
+++ benchmark/sqlbuilder.php
@@ -4,14 +4,24 @@ require( 'tests/bootstrap.php');
 #  $pdo = new PDO('mysql:host=localhost;dbname=benchmarks', 'root', '123123' );
 #  $mysqli = new mysqli("localhost", "root", "123123", "benchmarks");
 
+
+$driver = new SQLBuilder\Driver;
+$driver->configure('driver','postgresql');
+$driver->configure('quote_table',true);
+$driver->configure('quote_column',true);
+$driver->configure('trim',true);
+$driver->configure('placeholder','named');
+
+$sb = new SQLBuilder\CRUDBuilder;
+$sb->driver = $driver;
+
+
 $bench = new SimpleBench( array( 'gc' => 1 ));
 $bench->n = 10000;
 $bench->title = 'sql builder';
 
-$bench->iterate( 'pdo_query' , 'pdo' , function() use($pdo) {
-});
-
-$bench->iterate( 'pdo_prepare' , 'pdo' , function() use($pdo) {
+$bench->iterate( 'select' , 'select' , function() use($sb) {
+    $sb->select( '*' )->table('users')->alias('u')->build();
 });
 
 $result = $bench->compare();
