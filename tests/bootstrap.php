24ecad1100c5dc16a06850f84a10bf1f9bfe4258
Yo-An Lin <yoanlin93@gmail.com>
2012-01-31 13:36:07 +0800

    Add TestMore to package.ini

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-require 'PHPUnit_TestMore.php';
+require 'vendor/pear/PHPUnit_TestMore.php';
 require 'vendor/pear/Universal/ClassLoader/BasePathClassLoader.php';
 $loader = new \Universal\ClassLoader\BasePathClassLoader(array('src','vendor/pear'));
 $loader->register();
