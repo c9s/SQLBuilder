f8e4680a487bb82c6b047e5503c31f76c34b0d41
Yo-An Lin <yoanlin93@gmail.com>
2012-01-31 13:50:39 +0800

    Add join benchmark test

--- benchmark/sqlbuilder.php
+++ benchmark/sqlbuilder.php
@@ -17,13 +17,21 @@ $sb->driver = $driver;
 
 
 $bench = new SimpleBench( array( 'gc' => 1 ));
-$bench->n = 10000;
+$bench->n = 100;
 $bench->title = 'sql builder';
 
 $bench->iterate( 'select' , 'select' , function() use($sb) {
     $sb->select( '*' )->table('users')->alias('u')->build();
 });
 
+$bench->iterate( 'join' , 'join' , function() use($sb) {
+    $sb->select( '*' )
+        ->table('users')->alias('u')
+        ->join('tweets')
+            ->alias('t')
+            ->on()
+                ->equal('u.id',array('t.user_id'))->back()->build();
+});
+
 $result = $bench->compare();
 echo $result->output('console');
-
