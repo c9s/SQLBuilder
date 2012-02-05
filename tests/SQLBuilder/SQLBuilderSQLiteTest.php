354deb8dc17cbba324cd571d0069d4dcd7e28c36
c9s <cornelius.howl@gmail.com>
2012-02-06 03:08:32 +0800

    more sqlite tests

--- tests/SQLBuilder/SQLBuilderSQLiteTest.php
+++ tests/SQLBuilder/SQLBuilderSQLiteTest.php
@@ -41,7 +41,7 @@ class SQLBuilderSQLiteTest extends PHPUnit_Framework_TestCase
 
     function testInsert()
     {
-        $pdo = new PDO('sqlite::memory');
+        $pdo = new PDO('sqlite::memory:');
 
         $sb = new SQLBuilder\QueryBuilder;
         $sb->table('member');
@@ -52,7 +52,39 @@ class SQLBuilderSQLiteTest extends PHPUnit_Framework_TestCase
         ));
         $sql = $sb->build();
 
-        
+
+    }
+
+    function testGroupBy()
+    {
+        $pdo = new PDO('sqlite::memory:');
+        $pdo->query( 'create table member ( name varchar(128) , phone varchar(128) , country varchar(128) );' );
+        ok( $pdo );
+
+        $stm = $pdo->prepare('insert into member ( name, phone, country ) values ( :name, :phone, :country ) ');
+        $countries = array('Taiwan','Japan','China','Taipei');
+
+        foreach( $countries as $country ) {
+            foreach( range(1,20) as $i ) {
+                $stm->execute(array( $i , $i , $country ));
+            }
+        }
+
+        $sb = new SQLBuilder\QueryBuilder;
+        $sb->driver = $this->getDriver();
+        $sb->table('member')->select('name')
+            ->groupBy('country')
+            ->order('name');
+        $sql = $sb->build();
+        $stm = $pdo->query( $sql );
+
+        $err = $pdo->errorInfo();
+        ok( $err[1] == null );
+
+        $row = $stm->fetch();
+        $row2 = $stm->fetch();
+        ok( $row );
+        ok( $row2 );
     }
 }
 
