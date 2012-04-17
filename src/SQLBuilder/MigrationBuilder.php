bc0d9e2f670eeb527a649c442f97686d85d12d97
Yo-An Lin <yoanlin93@gmail.com>
2012-04-18 00:17:49 +0800

    Add MigrationBuilder tests

--- src/SQLBuilder/MigrationBuilder.php
+++ src/SQLBuilder/MigrationBuilder.php
@@ -1,6 +1,92 @@
 <?php
 namespace SQLBuilder;
 
+class Column
+{
+    public $name;
+
+    public $attributes = array();
+
+    static function create($name)
+    {
+        return new self($name);
+    }
+
+    function __construct($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    function __set($n,$v)
+    {
+        $this->attributes[ $n ] = $v;
+    }
+
+    function __isset($n)
+    {
+        return isset( $this->attributes[ $n ] );
+    }
+
+    function __get($n)
+    {
+        if( isset( $this->attributes[ $n ] ) )
+            return $this->attributes[ $n ];
+    }
+
+    function __call($m,$a)
+    {
+        if( empty($a) ) {
+            $this->attributes[ $m ] = true;
+        }
+        elseif( count($a) > 1 ) {
+            $this->attributes[ $m ] = $a;
+        }
+        else {
+            $this->attributes[ $m ] = $a[0];
+        }
+        return $this;
+    }
+
+    function timestamp()
+    {
+        $this->type = 'timestamp';
+        return $this;
+    }
+
+    function varchar($length)
+    {
+        $this->type = "varchar($length)";
+        return $this;
+    }
+
+    function blob()
+    {
+        $this->type = 'blob';
+        return $this;
+    }
+
+    function integer()
+    {
+        $this->type = $integer;
+        return $this;
+    }
+
+
+    function isNull()
+    {
+        $this->isNotNull = true;
+        return $this;
+    }
+
+    function isNotNull()
+    {
+        $this->notNull = true;
+        return $this;
+    }
+
+}
+
 class MigrationBuilder
 {
     public $driver;
@@ -11,26 +97,25 @@ class MigrationBuilder
         $this->driver = $driver;
     }
 
-    public function addColumn( $table, $column , $attributes = array() ) 
+    public function addColumn( $table, $column ) 
     {
         $sql = 'alter table ' . $this->driver->getQuoteTableName( $table )
-             . ' add column ' . $this->driver->getQuoteColumn( $column );
+             . ' add column ' . $this->driver->getQuoteColumn( $column->name );
 
         // build attributes
-        if( isset($attributes['type']) ) {
-            $sql .= ' ' . $attributes['type'];
+        if( isset($column->type) ) {
+            $sql .= ' ' . $column->type;
         }
 
-        if( isset($attributes['default']) ) {
-            $sql .= ' default ' . $attributes['default'];
+        if( isset($column->default) ) {
+            $sql .= ' default ' . $column->default;
         }
 
-        if( isset($attributes['is_null'] && $attributes['is_null'] ) ) {
+        if( $column->isNull ) {
             $sql .= ' is null';
         }
-        else {
+        elseif( $column->isNotNull ) {
             $sql .= ' is not null';
-
         }
     }
 }
--- tests/SQLBuilder/MigrationBuilderTest.php
+++ tests/SQLBuilder/MigrationBuilderTest.php
@@ -1,10 +1,32 @@
 <?php
+use SQLBuilder\Column;
+
+class ColumnTest extends PHPUnit_Framework_TestCase
+{
+    function testTimestamp()
+    {
+        $createdOn = Column::create('created_on');
+        $createdOn->type('timestamp')
+            ->default(array('current_timestamp'))
+            ->notNull();
+        ok( $createdOn );
+    }
+
+}
+
 
 class MigrationBuilderTest extends PHPUnit_Framework_TestCase
 {
+
     function test()
     {
+        $driver = DriverFactory::create_sqlite_driver();
 
+        $builder = new SQLBuilder\MigrationBuilder( $driver );
+        $sql = $builder->addColumn( 'product' , 
+            SQLBuilder\Column::create('price')
+                ->default(100)
+        );
         
     }
 }
