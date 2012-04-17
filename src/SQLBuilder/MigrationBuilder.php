f25bf15472ccc50eb36c26e9b695b7d1980f3005
c9s <cornelius.howl@gmail.com>
2012-04-17 23:49:11 +0800

    Update for basic migration builder

--- src/SQLBuilder/MigrationBuilder.php
+++ src/SQLBuilder/MigrationBuilder.php
@@ -3,6 +3,38 @@ namespace SQLBuilder;
 
 class MigrationBuilder
 {
+    public $driver;
+
+
+    public function __construct($driver)
+    {
+        $this->driver = $driver;
+    }
+
+    public function addColumn( $table, $column , $attributes = array() ) 
+    {
+        $sql = 'alter table ' . $this->driver->getQuoteTableName( $table )
+             . ' add column ' . $this->driver->getQuoteColumn( $column );
+
+        // build attributes
+        if( isset($attributes['type']) ) {
+            $sql .= ' ' . $attributes['type'];
+        }
+
+        if( isset($attributes['default']) ) {
+            $sql .= ' default ' . $attributes['default'];
+        }
+
+        if( isset($attributes['is_null'] && $attributes['is_null'] ) ) {
+            $sql .= ' is null';
+        }
+        else {
+            $sql .= ' is not null';
+
+        }
+
+    }
+
 
 }
 
