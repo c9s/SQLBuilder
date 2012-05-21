4d40e3e077976627e67ce203c5b74d9664e3d25f
c9s <cornelius.howl@gmail.com>
2012-05-21 16:26:01 +0800

    Update TestCase

--- tests/PHPUnit/PDO/TestCase.php
+++ tests/PHPUnit/PDO/TestCase.php
@@ -1,4 +1,25 @@
 <?php
+
+
+/**
+ * Usage
+ *
+ *   class YourTest extends PHPUnit_PDO_TestCase
+ *   {
+ *
+ *      // setup your database connection DSN (optional, default is sqlite memory)
+ *      public $dsn = 'pgsql:tests';
+ *
+ *      // setup your database username (optional)
+ *      public $user = 'postgres';
+ *
+ *      // setup your database password (optional)
+ *      public $pass = 'postgres';
+ *
+ *      public $options = array( ... PDO connection options ... );
+ *
+ *   }
+ */
 abstract class PHPUnit_PDO_TestCase extends PHPUnit_Framework_TestCase
 {
     public $pdo;
@@ -6,12 +27,43 @@ abstract class PHPUnit_PDO_TestCase extends PHPUnit_Framework_TestCase
     public $dsn = 'sqlite::memory:';
     public $user;
     public $pass;
+    public $options;
 
     public function noPDOError()
     {
         $err = $this->pdo->errorInfo();
         ok( $err[0] === '00000' );
     }
+
+    public function setup()
+    {
+        $this->pdo = new PDO(
+            $this->dsn,  
+            $this->user,  
+            $this->pass, 
+            $this->options ?: null
+        );
+
+        // throw Exception on Error.
+        $this->pdo->setAttribute( PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION );
+        $this->setupSchema();
+    }
+
+    public function setupSchema()
+    {
+        // get schema SQL and send query
+        $sqls = $this->schema();
+        foreach( $sqls as $sql ) {
+            $this->pdo->query($sql);
+        }
+        // well done!
+    }
+
+    public function testConnection() 
+    {
+        $this->assertInstanceOf('PDO', $this->pdo);
+    }
+
 
     public function schema()
     {
@@ -28,15 +80,6 @@ SQL;
         return $sqls;
     }
 
-    public function setup()
-    {
-        $this->pdo = new PDO($this->dsn,$this->user,$this->pass);
-        $this->pdo->setAttribute( PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION );
-        $sqls = $this->schema();
-        foreach( $sqls as $sql ) {
-            $this->pdo->query($sql);
-        }
-    }
 
     public function queryOk($sql, $args = null)
     {
