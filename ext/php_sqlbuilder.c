fac53edaf2460dc3715ff37a40da06923d2c119f
c9s <cornelius.howl@gmail.com>
2013-03-20 09:17:34 +0800

    Import sqlbuilder extension skeleton

---
+++ ext/php_sqlbuilder.c
@@ -0,0 +1,36 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+#include "php.h"
+#include "php_sqlbuilder.h"
+
+static function_entry sqlbuilder_functions[] = {
+    PHP_FE(sqlbuilder_test, NULL)
+    {NULL, NULL, NULL}
+};
+
+zend_module_entry sqlbuilder_module_entry = {
+#if ZEND_MODULE_API_NO >= 20010901
+    STANDARD_MODULE_HEADER,
+#endif
+    PHP_SQLBUILDER_EXTNAME,
+    sqlbuilder_functions,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+#if ZEND_MODULE_API_NO >= 20010901
+    PHP_SQLBUILDER_VERSION,
+#endif
+    STANDARD_MODULE_PROPERTIES
+};
+
+#ifdef COMPILE_DL_SQLBUILDER
+ZEND_GET_MODULE(sqlbuilder)
+#endif
+
+PHP_FUNCTION(sqlbuilder_test)
+{
+    RETURN_STRING("Hello World", 1);
+}
