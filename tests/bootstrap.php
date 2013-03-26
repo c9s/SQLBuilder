d073cdc64281fe1947c40cc12c8b6066791c58b3
Yo-An Lin <yoanlin93@gmail.com>
2013-03-27 01:11:31 +0800

    Fix SQLBuilder autoload for composer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,4 @@
 <?php
-require 'PHPUnit/TestMore.php';
-require 'vendor/pear/Universal/ClassLoader/BasePathClassLoader.php';
+$loader = require 'vendor/autoload.php';
 require 'tests/DriverFactory.php';
 require 'tests/PHPUnit/PDO/TestCase.php';
-$loader = new \Universal\ClassLoader\BasePathClassLoader(array('src','vendor/pear'));
-$loader->useIncludePath(true);
-// prepend to the spl class loader list
-$loader->register(true); // use prepend
