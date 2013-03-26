162f17529cd57fda7f32000fa49951e41c3b3a4d
Yo-An Lin <yoanlin93@gmail.com>
2013-03-27 01:12:07 +0800

    Set xhprof ini

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,3 +2,7 @@
 $loader = require 'vendor/autoload.php';
 require 'tests/DriverFactory.php';
 require 'tests/PHPUnit/PDO/TestCase.php';
+
+if (extension_loaded('xhprof') ) {
+    ini_set('xhprof.output_dir','/tmp');
+}
