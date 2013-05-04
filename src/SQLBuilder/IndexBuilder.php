754174b7dc458c9f04b4f6416ad478db98478c8f
c9s <cornelius.howl@gmail.com>
2013-05-04 19:00:49 +0800

    Add IndexBuilder class.

---
+++ src/SQLBuilder/IndexBuilder.php
@@ -0,0 +1,161 @@
+<?php
+namespace SQLBuilder;
+
+class IndexBuilder
+{
+    public $driver;
+
+    public function __construct($driver)
+    {
+        $this->driver = $driver;
+    }
+
+    /**
+     * pgsql create index:
+     * @link http://www.postgresql.org/docs/8.2/static/sql-createindex.html
+     *
+     * mysql:
+     * @link http://dev.mysql.com/doc/refman/5.0/en/create-index.html
+     */
+    public function createIndex($table,$indexName,$columnNames)
+    {
+        $self = $this;
+        $sql = 'CREATE INDEX ' . $this->driver->getQuoteTableName($indexName) 
+            . ' ON ' . $this->driver->getQuoteTableName($table);
+        if( is_array($columnNames) ) {
+            $sql .= ' (' . join(',' , array_map( function($name) use ($self) { 
+                                        return $self->driver->getQuoteColumn( $name );
+                                    }, $columnNames ) )
+                . ')';
+        }
+        else {
+            $sql .= ' (' . $this->driver->getQuoteColumn( $columnNames ) . ')';
+        }
+        return $sql;
+    }
+
+
+    /**
+     * Add reference
+
+     PostgreSQL version:
+
+        ALTER TABLE products ADD FOREIGN KEY (product_group_id) REFERENCES product_groups;
+        ALTER TABLE employee ADD FOREIGN KEY (group_id) REFERENCES product_groups;
+        ALTER TABLE items add foreign key (vendor_id) references vendors(vendor_code);
+
+     Works for PostgreSQL and MySQL
+
+        ALTER TABLE items ADD COLUMN vendor_id integer REFERENCES vendors(vendor_code);
+
+        http://www.postgresql.org/docs/8.1/static/ddl-alter.html
+
+    SQL-92 syntax
+
+        ALTER TABLE child ADD CONSTRAINT fk_child_parent
+                    FOREIGN KEY (parent_id) 
+                    REFERENCES child(id);
+
+    SQLite ??? (is not supported)
+
+    Usage:
+
+        $migration->addForeignKey('product_id','products');
+        $migration->addForeignKey('product_id','products','id');
+
+
+     */
+    public function addForeignKey($table,$columnName,$referenceTable,$referenceColumn = null) 
+    {
+        // SQLite doesn't support ADD CONSTRAINT
+        if( 'sqlite' === $this->driver->type ) {
+            return '';
+        }
+
+        // ALTER TABLE employee ADD FOREIGN KEY (group_id) REFERENCES product_groups;
+        $sql = 'ALTER TABLE ' 
+            . $this->driver->getQuoteTableName($table)
+            . ' ADD FOREIGN KEY '
+            . '(' . $this->driver->getQuoteTableName($columnName) . ')'
+            . ' REFERENCES '
+            . $this->driver->getQuoteTableName($table)
+            . ( $referenceColumn ? '(' . $this->driver->getQuoteColumn($referenceColumn) . ')' : '' )
+            ;
+        return $sql;
+    }
+
+    /**
+     * Add reference
+
+     PostgreSQL version:
+
+        ALTER TABLE products ADD FOREIGN KEY (product_group_id) REFERENCES product_groups;
+        ALTER TABLE employee ADD FOREIGN KEY (group_id) REFERENCES product_groups;
+        ALTER TABLE items add foreign key (vendor_id) references vendors(vendor_code);
+
+     Works for PostgreSQL and MySQL
+
+        ALTER TABLE items ADD COLUMN vendor_id integer REFERENCES vendors(vendor_code);
+
+        http://www.postgresql.org/docs/8.1/static/ddl-alter.html
+
+    SQL-92 syntax
+
+        ALTER TABLE child ADD CONSTRAINT fk_child_parent
+                    FOREIGN KEY (parent_id) 
+                    REFERENCES child(id);
+
+    SQLite ??? (is not supported)
+
+    Usage:
+
+        $migration->addForeignKey('product_id','products');
+        $migration->addForeignKey('product_id','products','id');
+
+
+     */
+    public function addForeignKey($table,$columnName,$referenceTable,$referenceColumn = null) 
+    {
+        // SQLite doesn't support ADD CONSTRAINT
+        if( 'sqlite' === $this->driver->type ) {
+            return '';
+        }
+
+        // ALTER TABLE employee ADD FOREIGN KEY (group_id) REFERENCES product_groups;
+        $sql = 'ALTER TABLE ' 
+            . $this->driver->getQuoteTableName($table)
+            . ' ADD FOREIGN KEY '
+            . '(' . $this->driver->getQuoteTableName($columnName) . ')'
+            . ' REFERENCES '
+            . $this->driver->getQuoteTableName($table)
+            . ( $referenceColumn ? '(' . $this->driver->getQuoteColumn($referenceColumn) . ')' : '' )
+            ;
+        return $sql;
+    }
+
+
+    /**
+     * mysql
+     *
+     * @link http://dev.mysql.com/doc/refman/5.0/en/drop-index.html
+     */
+    public function dropIndex($table,$indexName)
+    {
+        $sql = '';
+        switch( $this->driver->type )
+        {
+            case 'mysql':
+                $sql = 'DROP INDEX ' 
+                    . $this->driver->getQuoteTableName($indexName) 
+                    . ' ON ' . $this->driver->getQuoteTableName($table);
+            break;
+            case 'sqlite':
+            case 'pgsql':
+                $sql = 'DROP INDEX ' . $this->driver->getQuoteTableName($indexName);
+            break;
+        }
+        return $sql;
+    }
+
+}
+
