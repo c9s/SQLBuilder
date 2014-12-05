8a3a6f64223dc4623a58d3036d63bfe6ee935dc5
c9s <yoanlin93@gmail.com>
2014-12-05 19:31:46 +0800

    Add ParamsExpr

---
+++ src/SQLBuilder/Expression/ParamsExpr.php
@@ -0,0 +1,35 @@
+<?php
+namespace SQLBuilder\Expression;
+
+use SQLBuilder\Expression\Expr;
+use SQLBuilder\Driver\BaseDriver;
+use SQLBuilder\ParamMarker;
+use LogicException;
+
+/**
+ * This class is used for handling function parameters
+ */
+class ParamsExpr extends Expr { 
+
+    public $params;
+
+    public function __construct(array $params) {
+        $this->params = $params;
+    }
+
+    public function append($val) {
+        $this->params[] = $val;
+    }
+
+    public function renderSet(BaseDriver $driver, array $set) {
+        return array_map(function($val) use($driver) {
+            return $driver->deflate($val);
+        }, $set);
+    }
+
+    public function toSql(BaseDriver $driver) {
+        return join(',', $this->renderSet($driver, $this->params));
+    }
+
+}
+
