7123d37b49823ecd38fa3d7d339c8d766b5324d6
c9s <yoanlin93@gmail.com>
2014-12-05 19:27:19 +0800

    Implement ListExpr class to handle (a,b,...) syntax

--- src/SQLBuilder/Expression/InExpr.php
+++ src/SQLBuilder/Expression/InExpr.php
@@ -1,30 +1,22 @@
 <?php
 namespace SQLBuilder\Expression;
 use SQLBuilder\Expression\Expr;
+use SQLBuilder\Expression\ListExpr;
 use SQLBuilder\Driver\BaseDriver;
 use SQLBuilder\ParamMarker;
 use LogicException;
 
 class InExpr extends Expr { 
 
-    public $set = array();
+    public $listExpr;
 
     public function __construct($exprStr, array $set)
     {
         $this->exprStr = $exprStr;
-        $this->set = $set;
-    }
-
-    public function renderSet(BaseDriver $driver, array $set) {
-        $values = array();
-        foreach($set as $val) {
-            $values[] = $driver->deflate($val);
-        }
-        return $values;
+        $this->listExpr = new ListExpr($set);
     }
 
     public function toSql(BaseDriver $driver) {
-        // TODO: check instance (ParamMarker or Variable) and quote the string if need
-        return $this->exprStr . ' IN (' . join(',', $this->renderSet($driver, $this->set)) . ')';
+        return $this->exprStr . ' IN ' . $this->listExpr->toSql($driver);
     }
 }
