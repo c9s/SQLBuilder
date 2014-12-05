853af37945122376ca83ce64c8444814e4c70f95
c9s <yoanlin93@gmail.com>
2014-12-06 02:01:56 +0800

    Support IsExpr

--- src/SQLBuilder/Expression/NotInExpr.php
+++ src/SQLBuilder/Expression/NotInExpr.php
@@ -3,7 +3,7 @@ namespace SQLBuilder\Expression;
 use SQLBuilder\Expression\Expr;
 use SQLBuilder\Driver\BaseDriver;
 
-class NotInExpr extends InExpr { 
+class NotInExpr extends InExpr {
 
     public function toSql(BaseDriver $driver) {
         return $this->exprStr . ' NOT IN ' . $this->listExpr->toSql($driver);
