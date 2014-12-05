6ad5bc577b0f70808d01fd06851ce8d185a61416
c9s <yoanlin93@gmail.com>
2014-12-06 01:33:40 +0800

    Rename ExpressionBuilder to ConditionsExpr

--- tests/SQLBuilder/Expression/ConditionsExprTest.php
+++ tests/SQLBuilder/Expression/ConditionsExprTest.php
@@ -1,14 +1,14 @@
 <?php
-use SQLBuilder\ExpressionBuilder;
+use SQLBuilder\Expression\ConditionsExpr;
 use SQLBuilder\Criteria;
 
-class ExpressionBuilderTest extends PHPUnit_Framework_TestCase
+class ConditionsExprTest extends PHPUnit_Framework_TestCase
 {
     public function testAppendExpr() {
         $driver = new SQLBuilder\Driver\MySQLDriver;
         ok($driver);
 
-        $exprBuilder = new ExpressionBuilder;
+        $exprBuilder = new ConditionsExpr;
         $exprBuilder->appendExpr('a', '=', 123);
         $sql = $exprBuilder->toSql($driver);
         is("a = 123",$sql);
@@ -16,7 +16,7 @@ class ExpressionBuilderTest extends PHPUnit_Framework_TestCase
 
     public function testInExpr() {
         $driver = new SQLBuilder\Driver\MySQLDriver;
-        $expr = new ExpressionBuilder;
+        $expr = new ConditionsExpr;
         $expr->in('b', [ 'a', 'b', 'c' ]);
         $sql = $expr->toSql($driver);
         is("b IN ('a','b','c')", $sql);
@@ -24,7 +24,7 @@ class ExpressionBuilderTest extends PHPUnit_Framework_TestCase
 
     public function testNotInExpr() {
         $driver = new SQLBuilder\Driver\MySQLDriver;
-        $expr = new ExpressionBuilder;
+        $expr = new ConditionsExpr;
         $expr->notIn('z', [ 'a', 'b', 'c' ]);
         $sql = $expr->toSql($driver);
         is("z NOT IN ('a','b','c')", $sql);
@@ -32,7 +32,7 @@ class ExpressionBuilderTest extends PHPUnit_Framework_TestCase
 
     public function testEqual() {
         $driver = new SQLBuilder\Driver\MySQLDriver;
-        $expr = new ExpressionBuilder;
+        $expr = new ConditionsExpr;
         $expr->equal('a', 1);
         $sql = $expr->toSql($driver);
         is("a = 1", $sql);
@@ -40,7 +40,7 @@ class ExpressionBuilderTest extends PHPUnit_Framework_TestCase
 
     public function testNotEqual() {
         $driver = new SQLBuilder\Driver\MySQLDriver;
-        $expr = new ExpressionBuilder;
+        $expr = new ConditionsExpr;
         $expr->notEqual('a', 1);
         $sql = $expr->toSql($driver);
         is("a <> 1", $sql);
@@ -61,7 +61,7 @@ class ExpressionBuilderTest extends PHPUnit_Framework_TestCase
      */
     public function testLikeExpr($criteria, $pat, $expectedSql) {
         $driver = new SQLBuilder\Driver\MySQLDriver;
-        $expr = new ExpressionBuilder;
+        $expr = new ConditionsExpr;
         $expr->like('name', $pat, $criteria);
         $sql = $expr->toSql($driver);
         is($expectedSql, $sql);
@@ -70,7 +70,7 @@ class ExpressionBuilderTest extends PHPUnit_Framework_TestCase
     public function testBetweenExpr()
     {
         $driver = new SQLBuilder\Driver\MySQLDriver;
-        $expr = new ExpressionBuilder;
+        $expr = new ConditionsExpr;
         $expr->between('created_at', date('c') , date('c', time() + 3600));
         $sql = $expr->toSql($driver);
         // is("", $sql);
