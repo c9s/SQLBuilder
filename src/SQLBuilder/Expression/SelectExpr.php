f61ac3266db2b005da7d25d72e84013c41ca1d21
c9s <yoanlin93@gmail.com>
2014-12-07 11:28:12 +0800

    Add SelectExpr for "select {expr} as col_name"

---
+++ src/SQLBuilder/Expression/SelectExpr.php
@@ -0,0 +1,35 @@
+<?php
+namespace SQLBuilder\Expression;
+use SQLBuilder\Expression\Expr;
+use SQLBuilder\Driver\BaseDriver;
+use SQLBuilder\ParamMarker;
+use SQLBuilder\Criteria;
+use SQLBuilder\ArgumentArray;
+use SQLBuilder\ToSqlInterface;
+use LogicException;
+
+class SelectExpr extends Expr implements ToSqlInterface { 
+
+    public $expr;
+
+    public $as;
+
+    public function __construct($expr, $as = NULL)
+    {
+        $this->expr = $expr; // XXX: could be a Function call expr
+        $this->as = $as;
+    }
+
+    public function deflateExpr($expr, BaseDriver $driver, ArgumentArray $args) {
+        return $expr instanceof ToSqlInterface ? $expr->toSql($driver $args) : $expr;
+    }
+
+    public function toSql(BaseDriver $driver, ArgumentArray $args) {
+        if ($this->as) {
+            return $this->expr . ' AS ' . $this->as;
+        } else {
+            return $this->expr . ' AS ' . $this->as;
+        }
+    }
+
+}
