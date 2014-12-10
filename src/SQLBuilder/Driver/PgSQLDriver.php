f9545cf6d64efe5873342d5cb24b7e267354b2d0
c9s <yoanlin93@gmail.com>
2014-12-10 18:44:17 +0800

    simplify quote interface

--- src/SQLBuilder/Driver/PgSQLDriver.php
+++ src/SQLBuilder/Driver/PgSQLDriver.php
@@ -6,41 +6,8 @@ use RuntimeException;
 
 class PgSQLDriver extends BaseDriver
 {
-
-    /**
-     * Check driver optino to quote table name
-     *
-     * column quote can be configured by 'quote_table' option.
-     *
-     * @param string $name table name
-     * @return string table name with/without quotes.
-     */
-    public function quoteTableName($name) 
-    {
-        if ($this->quoteTable) {
-            return '"' . $name . '"';
-        }
-        return $name;
+    public function quoteIdentifier($id) {
+        return '"' . $id . '"';
     }
-
-    /**
-     * Check driver option to quote column name
-     *
-     * column quote can be configured by 'quote_column' option.
-     *
-     * @param string $name column name
-     * @return string column name with/without quotes.
-     */
-    public function quoteColumn($name)
-    {
-        if ($c = $this->quoteColumn) {
-            if (preg_match('/\W/',$name)) {
-                return $name;
-            }
-            return '"' . $name . '"';
-        }
-        return $name;
-    }
-    
 }
 
