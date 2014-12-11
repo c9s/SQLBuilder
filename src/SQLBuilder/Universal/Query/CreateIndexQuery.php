4456259b328252715a533bea6879fa2da92d41d8
c9s <yoanlin93@gmail.com>
2014-12-12 01:23:54 +0800

    Document example queries

--- src/SQLBuilder/Universal/Query/CreateIndexQuery.php
+++ src/SQLBuilder/Universal/Query/CreateIndexQuery.php
@@ -42,6 +42,29 @@ CREATE [ UNIQUE ] INDEX [ CONCURRENTLY ] name ON table [ USING method ]
     [ TABLESPACE tablespace ]
     [ WHERE predicate ]
 
+
+Example Queries
+---------------
+
+    CREATE INDEX ON films ((lower(title)));
+
+    CREATE INDEX title_idx_german ON films (title COLLATE "de_DE");
+
+    CREATE INDEX title_idx_nulls_low ON films (title NULLS FIRST);
+
+
+    CREATE INDEX pointloc
+        ON points USING gist (box(location,location));
+    SELECT * FROM points
+        WHERE box(location,location) && '(0,0),(1,1)'::box;
+
+    CREATE INDEX CONCURRENTLY sales_quantity_index ON sales_table (quantity);
+
+    CREATE INDEX code_idx ON films (code) TABLESPACE indexspace;
+
+    CREATE UNIQUE INDEX title_idx ON films (title) WITH (fillfactor = 70);
+    CREATE INDEX gin_idx ON documents_table USING gin (locations) WITH (fastupdate = off);
+    
  */
 
 class CreateIndexQuery implements ToSqlInterface
