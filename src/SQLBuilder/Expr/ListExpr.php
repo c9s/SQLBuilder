20283360e897ab5f2aeb3c8d2cb7f512953a88f8
c9s <yoanlin93@gmail.com>
2014-12-11 23:13:05 +0800

    Remove ParamsExpr dependency

--- src/SQLBuilder/Expr/ListExpr.php
+++ src/SQLBuilder/Expr/ListExpr.php
@@ -8,11 +8,32 @@ use SQLBuilder\ToSqlInterface;
 use SQLBuilder\ArgumentArray;
 use LogicException;
 
-class ListExpr extends ParamsExpr implements ToSqlInterface
+class ListExpr implements ToSqlInterface
 {
+    public $params;
+
+    public function __construct(array $params) {
+        $this->params = $params;
+    }
+
+    public function append($val) {
+        $this->params[] = $val;
+    }
+
+    public function renderSet(BaseDriver $driver, ArgumentArray $args, array $set) 
+    {
+        return array_map(function($val) use($driver, $args) {
+            return $driver->deflate($val, $args);
+        }, $set);
+    }
+
     public function toSql(BaseDriver $driver, ArgumentArray $args)
     {
-        return '(' . parent::toSql($driver, $args) . ')';
+        $sql = '';
+        foreach($this->params as $val) {
+            $sql .= ',' . $driver->deflate($val, $args);
+        }
+        return '(' . ltrim($sql, ',') . ')';
     }
 }
 
