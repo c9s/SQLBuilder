6866105482cecf9f43ce2437fc5b665158f5c092
c9s <yoanlin93@gmail.com>
2014-12-12 01:15:29 +0800

    Update TestCase with QueryTestCase

--- tests/SQLBuilder/MySQL/Query/CreateUserQueryTest.php
+++ tests/SQLBuilder/MySQL/Query/CreateUserQueryTest.php
@@ -3,66 +3,53 @@ use SQLBuilder\Driver\MySQLDriver;
 use SQLBuilder\Driver\BaseDriver;
 use SQLBuilder\ArgumentArray;
 use SQLBuilder\MySQL\Query\CreateUserQuery;
+use SQLBuilder\Testing\QueryTestCase;
 
-class CreateUserQueryTest extends PHPUnit_Framework_TestCase
+class CreateUserQueryTest extends QueryTestCase
 {
+    public function createDriver() { 
+        return new MySQLDriver;
+    }
+
     public function testCreateSingleUser()
     {
-        $driver = new MySQLDriver;
-        $args = new ArgumentArray;
         $q = new CreateUserQuery;
         $q->user()->account('monty')->host('localhost')->identifiedBy('some_pass');
-        $sql = $q->toSql($driver, $args);
-        is("CREATE USER `monty`@`localhost` IDENTIFIED BY 'some_pass'", $sql);
+        $this->assertSql("CREATE USER `monty`@`localhost` IDENTIFIED BY 'some_pass'", $q);
     }
 
     public function testCreateUserWithSpecString() {
-        $driver = new MySQLDriver;
-        $args = new ArgumentArray;
         $q = new CreateUserQuery;
         $q->user('monty@localhost')->identifiedBy('some_pass');
-        $sql = $q->toSql($driver, $args);
-        is("CREATE USER `monty`@`localhost` IDENTIFIED BY 'some_pass'", $sql);
+        $this->assertSql("CREATE USER `monty`@`localhost` IDENTIFIED BY 'some_pass'", $q);
     }
 
     public function testCreateUserWithSpecString2() {
-        $driver = new MySQLDriver;
-        $args = new ArgumentArray;
         $q = new CreateUserQuery;
         $q->user('`monty`@`localhost`')->identifiedBy('some_pass');
-        $sql = $q->toSql($driver, $args);
-        is("CREATE USER `monty`@`localhost` IDENTIFIED BY 'some_pass'", $sql);
+        $this->assertSql("CREATE USER `monty`@`localhost` IDENTIFIED BY 'some_pass'", $q);
     }
 
     public function testCreateUserWithHashPassword() {
-        $driver = new MySQLDriver;
-        $args = new ArgumentArray;
         $q = new CreateUserQuery;
         $q->user('`monty`@`localhost`')->identifiedBy('*90E462C37378CED12064BB3388827D2BA3A9B689', true);
-        $sql = $q->toSql($driver, $args);
-        is("CREATE USER `monty`@`localhost` IDENTIFIED BY PASSWORD '*90E462C37378CED12064BB3388827D2BA3A9B689'", $sql);
+        $this->assertSql("CREATE USER `monty`@`localhost` IDENTIFIED BY PASSWORD '*90E462C37378CED12064BB3388827D2BA3A9B689'", $q);
     }
 
 
     public function testCreateSingleUserWithAuthPlugin()
     {
-        $driver = new MySQLDriver;
-        $args = new ArgumentArray;
         $q = new CreateUserQuery;
         $q->user()->account('monty')->host('localhost')->identifiedWith('mysql_native_password');
-        $sql = $q->toSql($driver, $args);
-        is("CREATE USER `monty`@`localhost` IDENTIFIED WITH `mysql_native_password`", $sql);
+        $this->assertSql("CREATE USER `monty`@`localhost` IDENTIFIED WITH `mysql_native_password`", $q);
     }
 
     public function testCreateMultipleUser()
     {
-        $driver = new MySQLDriver;
-        $args = new ArgumentArray;
         $q = new CreateUserQuery;
         $q->user()->account('monty')->host('localhost')->identifiedBy('some_pass');
         $q->user()->account('john')->host('%')->identifiedBy('some_pass');
-        $sql = $q->toSql($driver, $args);
-        is("CREATE USER `monty`@`localhost` IDENTIFIED BY 'some_pass', `john`@`%` IDENTIFIED BY 'some_pass'", $sql);
+        $this->assertSql("CREATE USER `monty`@`localhost` IDENTIFIED BY 'some_pass', `john`@`%` IDENTIFIED BY 'some_pass'", $q);
     }
 }
 
