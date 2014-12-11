07b576ffb4763baab62f2fdf1cc077a22077fd14
c9s <yoanlin93@gmail.com>
2014-12-12 01:41:28 +0800

    Improve drop index sytnax for MySQL 5.6 and PostgreSQL

--- src/SQLBuilder/Universal/Query/DropIndexQuery.php
+++ src/SQLBuilder/Universal/Query/DropIndexQuery.php
@@ -10,6 +10,7 @@ use SQLBuilder\Driver\PgSQLDriver;
 use SQLBuilder\Exception\CriticalIncompatibleUsageException;
 use SQLBuilder\Exception\IncompleteSettingsException;
 use SQLBuilder\Exception\UnsupportedDriverException;
+use SQLBuilder\PgSQL\Traits\ConcurrentlyTrait;
 
 /*
 MySQL Drop Index Syntax
@@ -19,10 +20,21 @@ MySQL Drop Index Syntax
 */
 class DropIndexQuery implements ToSqlInterface
 {
+    use ConcurrentlyTrait;
 
     protected $indexName;
 
     protected $tableName;
+
+    /**
+     * MySQL
+     */
+    protected $lockType;
+
+    /**
+     * MySQL
+     */
+    protected $algorithm;
 
     public function drop($indexName) {
         $this->indexName = $indexName;
@@ -40,12 +52,51 @@ class DropIndexQuery implements ToSqlInterface
         return $this;
     }
 
-    public function concurrently() {
+    /**
+     * MySQL 5.6.6
+     *
+     * valid values: {DEFAULT|NONE|SHARED|EXCLUSIVE}
+     */
+    public function lock($lockType) {
+        $this->lockType = $lockType;
+        return $this;
+    }
 
+    /**
+     * MySQL 5.6.6
+     *
+     * valid values: {DEFAULT|INPLACE|COPY}
+     */
+    public function algorithm($algorithm) {
+        $this->algorithm = $algorithm;
+        return $this;
     }
 
     public function toSql(BaseDriver $driver, ArgumentArray $args) 
     {
-        return 'DROP INDEX ' . $driver->quoteIdentifier($this->indexName) . ' ON ' . $driver->quoteIdentifier($this->tableName);
+        $sql = 'DROP INDEX';
+
+        if ($driver instanceof PgSQLDriver) {
+            $sql .= $this->buildConcurrentlyClause();
+        }
+
+        $sql .= ' ' . $driver->quoteIdentifier($this->indexName);
+
+        if ($driver instanceof MySQLDriver) {
+            if (!$this->tableName) {
+                throw new IncompleteSettingsException('tableName is required. Use on($tableName) to specify one.');
+            }
+            $sql .= ' ON ' . $driver->quoteIdentifier($this->tableName);
+
+            if ($this->lockType) {
+                $sql .= ' LOCK = ' . $this->lockType;
+            }
+            if ($this->algorithm) {
+                $sql .= ' ALGORITHM = ' . $this->algorithm;
+            }
+
+        }
+
+        return $sql;
     }
 }
