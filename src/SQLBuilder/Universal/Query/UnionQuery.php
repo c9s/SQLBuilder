bc3c168a6e64221165cc4570362746ef592393dd
c9s <yoanlin93@gmail.com>
2014-12-12 16:11:22 +0800

    Add UnionQuery

---
+++ src/SQLBuilder/Universal/Query/UnionQuery.php
@@ -0,0 +1,33 @@
+<?php
+namespace SQLBuilder\Universal\Query;
+use SQLBuilder\Universal\Query\SelectQuery;
+use SQLBuilder\Driver\BaseDriver;
+use SQLBuilder\ArgumentArray;
+use SQLBuilder\ToSqlInterface;
+
+class UnionQuery implements ToSqlInterface
+{
+    public $queries = array();
+
+    public function union(SelectQuery $query) { 
+        $args = func_get_args();
+        if (count($args) == 1) {
+            $this->queries[] = $query;
+        } elseif (count($args) > 1) {
+            $this->queries = $this->queries + $args;
+        }
+        return $this;
+    }
+
+    public function toSql(BaseDriver $driver, ArgumentArray $args) 
+    {
+        $subqueries = array();
+        foreach($this->queries as $q) {
+            $subqueries[] = '(' . $q->toSql($driver, $args) . ')';
+        }
+        return join(' UNION ', $subqueries);
+    }
+}
+
+
+
