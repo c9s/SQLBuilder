0a4b3ef805b0e59b675ac861e00b59500bd8af63
c9s <yoanlin93@gmail.com>
2014-12-15 15:19:05 +0800

    Add CreateTableQuery class

--- src/SQLBuilder/ParamMarker.php
+++ src/SQLBuilder/ParamMarker.php
@@ -1,12 +1,22 @@
 <?php
 namespace SQLBuilder;
 
+/**
+ * ParamMarker is a data type for parameter mark without binding 
+ * value.
+ *
+ * Used for question mark and named mark
+ */
 class ParamMarker
 {
     public $name;
 
     public function __construct($name) {
         $this->name = $name;
+    }
+
+    public function getName() {
+        return $this->name;
     }
 
     public function getMark() {
