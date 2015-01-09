daa8a1399ecea13e7f90ffa2123952b6e3cd01ed
c9s <yoanlin93@gmail.com>
2015-01-09 23:09:26 +0800

    Support PgSQL rename

--- src/SQLBuilder/Universal/Syntax/AlterTableRenameColumn.php
+++ src/SQLBuilder/Universal/Syntax/AlterTableRenameColumn.php
@@ -28,6 +28,7 @@ class AlterTableRenameColumn implements ToSqlInterface
     {
         $sql = '';
         if ($driver instanceof MySQLDriver) {
+
             $sql = 'CHANGE COLUMN ';
             if (is_string($this->fromColumn)) {
                 $sql .= $driver->quoteIdentifier($this->fromColumn);
@@ -37,6 +38,20 @@ class AlterTableRenameColumn implements ToSqlInterface
 
             // the 'toColumn' must be a type of Column, we need at least column type to rename.
             $sql .= ' ' . $driver->quoteIdentifier($this->toColumn->getName()) . ' ' . $this->toColumn->getType();
+
+        } elseif ($driver instanceof PgSQLDriver) {
+
+            // ALTER TABLE distributors RENAME CONSTRAINT zipchk TO zip_check;
+            $sql = 'RENAME COLUMN ';
+            if (is_string($this->fromColumn)) {
+                $sql .= $driver->quoteIdentifier($this->fromColumn);
+            } elseif ($this->fromColumn instanceof Column) {
+                $sql .= $driver->quoteIdentifier($this->fromColumn->getName());
+            }
+
+            // the 'toColumn' must be a type of Column, we need at least column type to rename.
+            $sql .= ' TO ' . $driver->quoteIdentifier($this->toColumn->getName()) . ' ' . $this->toColumn->getType();
+
         } else {
             throw new UnsupportedDriverException;
         }
