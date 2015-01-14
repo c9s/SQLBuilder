08dbf7a051e2e71940af03ca203a9cc01bd6bdab
c9s <yoanlin93@gmail.com>
2015-01-14 23:53:38 +0800

    Add MySQL Type map

---
+++ src/SQLBuilder/MySQL/Types.php
@@ -0,0 +1,44 @@
+<?php
+namespace SQLBuilder\MySQL;
+use Exception;
+
+/**
+ *
+ * @author Yo-An Lin
+ * @copyright Yo-An Lin, 14 January, 2015
+ */
+class Types
+{
+    public $typemap = array(
+        'tinyint'    => true,
+        'smallint'   => true,
+        'mediumint'  => true,
+        'int'        => true,
+        'integer'    => true,
+        'bigint'     => true,
+        'int24'      => true,
+        'real'       => true,
+        'float'      => true,
+        'decimal'    => true,
+        'numeric'    => true,
+        'double'     => true,
+        'char'       => true,
+        'varchar'    => true,
+        'date'       => true,
+        'time'       => true,
+        'year'       => true,
+        'datetime'   => true,
+        'timestamp'  => true,
+        'tinyblob'   => true,
+        'blob'       => true,
+        'mediumblob' => true,
+        'longblob'   => true,
+        'longtext'   => true,
+        'tinytext'   => true,
+        'mediumtext' => true,
+        'text'       => true,
+        'enum'       => true,
+        'set'        => true,
+    );
+}
+
