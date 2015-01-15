06c4bf1536aecb87490272c8fa785517f2a0c360
c9s <yoanlin93@gmail.com>
2015-01-15 14:42:30 +0800

    Add Universal\Types class

---
+++ src/SQLBuilder/Universal/Types.php
@@ -0,0 +1,30 @@
+<?php
+namespace SQLBuilder\Universal;
+
+class Types
+{
+    const TINYINT     = 1;
+    const SMALLINT    = 2;
+    const INTEGER     = 3;
+    const MEDIUMINT   = 4;
+    const BIGINT      = 5;
+    const DOUBLE      = 6;
+    const FLOAT       = 7;
+    const DECIMAL     = 8;
+    const NUMERIC     = 9;
+    const CHAR        = 10;
+    const VARCHAR     = 11;
+    const DATE        = 12;
+    const TIME        = 13;
+    const TIMESTAMP   = 14;
+    const BINARY      = 15;
+    const VARBINARY   = 16;
+    const BLOB        = 17;
+    const LONGVARCHAR = 18;
+    const TEXT        = 19;
+}
+
+
+
+
+
