27b0e963a84c41844eb911ab7cc836226b059661
c9s <yoanlin93@gmail.com>
2015-01-15 15:49:07 +0800

    Remove first() and last() which is not in the standard functions

--- src/SQLBuilder/ANSI/AggregateFunction.php
+++ src/SQLBuilder/ANSI/AggregateFunction.php
@@ -5,31 +5,23 @@ use SQLBuilder\Raw;
 
 class AggregateFunction
 {
-    static public function avg($expr) {
+    static public function AVG($expr) {
         return new FuncCallExpr('AVG', array(new Raw($expr)));
     }
 
-    static public function count($expr) {
+    static public function COUNT($expr) {
         return new FuncCallExpr('COUNT', array(new Raw($expr)));
     }
 
-    static public function first($expr) {
-        return new FuncCallExpr('FIRST', array(new Raw($expr)));
-    }
-
-    static public function last($expr) {
-        return new FuncCallExpr('LAST', array(new Raw($expr)));
-    }
-
-    static public function max($expr) {
+    static public function MAX($expr) {
         return new FuncCallExpr('MAX', array(new Raw($expr)));
     }
 
-    static public function min($expr) {
+    static public function MIN($expr) {
         return new FuncCallExpr('MIN', array(new Raw($expr)));
     }
 
-    static public function sum($expr) {
+    static public function SUM($expr) {
         return new FuncCallExpr('SUM', array(new Raw($expr)));
     }
 }
