365e8e76954f13a71f307ccd10bf6dc7244358de
c9s <yoanlin93@gmail.com>
2015-01-17 01:33:42 +0800

    Update delete query test

--- tests/SQLBuilder/Universal/Query/DeleteQueryTest.php
+++ tests/SQLBuilder/Universal/Query/DeleteQueryTest.php
@@ -18,13 +18,15 @@ class DeleteQueryTest extends PDOQueryTestCase
         $query->delete('users', 'u')
             ->partitions('p1','p2')
             ->where()
-                ->equal('id', 3);
+                ->equal('id', 3)
+                ->is('confirmed', true)
+            ;
         $query->limit(1);
         
         $this->assertSqlStatements($query, [ 
-            [ new MySQLDriver, 'DELETE users AS u PARTITION (p1,p2) WHERE id = 3 LIMIT 1' ],
-            [ new PgSQLDriver, 'DELETE users AS u WHERE id = 3' ],
-            [ new SQLiteDriver, 'DELETE users AS u WHERE id = 3' ],
+            [ new MySQLDriver, 'DELETE users AS u PARTITION (p1,p2) WHERE id = 3 AND confirmed IS TRUE LIMIT 1' ],
+            [ new PgSQLDriver, 'DELETE users AS u WHERE id = 3 AND confirmed IS TRUE' ],
+            [ new SQLiteDriver, 'DELETE users AS u WHERE id = 3 AND confirmed IS 1' ],
         ]);
     }
 }
