e48a48ce61ce28e0d62cf7ce2290bab7336c9caf
c9s <yoanlin93@gmail.com>
2015-01-17 12:09:07 +0800

    Add IndexHintTrait

---
+++ src/SQLBuilder/MySQL/Traits/IndexHintTrait.php
@@ -0,0 +1,65 @@
+<?php
+namespace SQLBuilder\MySQL\Traits;
+use SQLBuilder\Raw;
+use SQLBuilder\Driver\BaseDriver;
+use SQLBuilder\Driver\MySQLDriver;
+use SQLBuilder\Driver\PgSQLDriver;
+use SQLBuilder\Driver\SQLiteDriver;
+use SQLBuilder\ToSqlInterface;
+use SQLBuilder\ArgumentArray;
+use SQLBuilder\Bind;
+use SQLBuilder\ParamMarker;
+use SQLBuilder\Universal\Syntax\IndexHint;
+
+trait IndexHintTrait {
+
+    /**
+     * @var IndexHint[] stored by list
+     */
+    protected $indexHints = array();
+
+    /**
+     * @var <string>IndexHint stored by tableRef
+     */
+    protected $indexHintsByTableRef = array();
+
+    public function indexHint($tableRef = NULL) {
+        $hint = new IndexHint($this);
+        $this->indexHints[] = $hint;
+        if ($tableRef) {
+            $this->indexHintsByTableRef[$tableRef][] = $hint;
+        }
+        return $hint;
+    }
+
+    public function definedIndexHint($tableRef)
+    {
+        return isset($this->indexHintsByTableRef[$tableRef]);
+    }
+
+    public function buildIndexHintClauseByTableRef($tableRef, BaseDriver $driver, ArgumentArray $args)
+    {
+        if (!isset($this->indexHintsByTableRef[$tableRef])) {
+            return '';
+        }
+        $sql = '';
+        foreach($this->indexHintsByTableRef[$tableRef] as $hint) {
+            $sql .= $hint->toSql($driver, $args);
+        }
+        return $sql;
+    }
+
+    public function buildIndexHintClause(BaseDriver $driver, ArgumentArray $args)
+    {
+        if (empty($this->indexHints)) {
+            return '';
+        }
+        $sql = '';
+        foreach($this->indexHints as $hint) {
+            $sql .= $hint->toSql($driver, $args);
+        }
+        return $sql;
+    }
+
+}
+
