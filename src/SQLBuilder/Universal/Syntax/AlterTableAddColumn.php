4c3212004b1109c169638f6689a76fd25685abf9
c9s <yoanlin93@gmail.com>
2015-04-11 16:26:27 +0800

    Support add column after .. syntax for mysql

--- src/SQLBuilder/Universal/Syntax/AlterTableAddColumn.php
+++ src/SQLBuilder/Universal/Syntax/AlterTableAddColumn.php
@@ -13,14 +13,31 @@ class AlterTableAddColumn implements ToSqlInterface
 {
     protected $column;
 
+    protected $after;
+
     public function __construct(Column $column) {
         $this->column = $column;
+    }
+
+    public function after($column) {
+        if ($column instanceof Column) {
+            $this->after = $column->getName();
+        } else {
+            $this->after = $column;
+        }
+        return $this;
     }
 
     public function toSql(BaseDriver $driver, ArgumentArray $args) 
     {
         $sql = 'ADD COLUMN ';
         $sql .= $this->column->buildDefinitionSql($driver, $args);
+
+        if ($driver instanceof MySQLDriver) {
+            if ($this->after) {
+                $sql .= ' AFTER ' . $driver->quoteIdentifier($this->after);
+            }
+        }
         return $sql;
     }
 }
