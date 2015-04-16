d67ea8f06ff01bbd07f4ccbf86c8892d7ff25021
c9s <yoanlin93@gmail.com>
2015-04-16 10:28:19 +0800

    Add MySQL AlterColumn Syntax support

---
+++ src/SQLBuilder/MySQL/Syntax/AlterTableAlterColumn.php
@@ -0,0 +1,55 @@
+<?php
+namespace SQLBuilder\MySQL\Syntax;
+use SQLBuilder\ToSqlInterface;
+use SQLBuilder\Driver\BaseDriver;
+use SQLBuilder\Driver\MySQLDriver;
+use SQLBuilder\Driver\PgSQLDriver;
+use SQLBuilder\ArgumentArray;
+use SQLBuilder\Universal\Traits\KeyTrait;
+use SQLBuilder\Universal\Syntax\Column;
+use SQLBuilder\Exception\UnsupportedDriverException;
+use InvalidArgumentException;
+
+class AlterTableAlterColumn implements ToSqlInterface
+{
+    protected $name;
+
+    protected $defaultValue;
+
+    protected $clauseType;
+
+    const SET_DEFAULT = 1;
+    const DROP_DEFAULT = 1;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setDefault($value)
+    {
+        $this->clauseType = self::SET_DEFAULT;
+        $this->defaultValue = $value;
+        return $this;
+    }
+
+    public function dropDefault()
+    {
+        $this->clauseType = self::DROP_DEFAULT;
+    }
+
+    public function toSql(BaseDriver $driver, ArgumentArray $args) 
+    {
+        if ($this->clauseType == self::SET_DEFAULT) {
+            return 'ALTER COLUMN' . $this->quoteIdentifier($this->name) . ' SET DEFAULT ' . $driver->deflate($this->defaultValue);
+        } else if ($this->clauseType == self::DROP_DEFAULT) {
+            return 'ALTER COLUMN' . $this->quoteIdentifier($this->name) . ' DROP DEFAULT';
+        } else {
+            throw new InvalidArgumentException('You should call either setDefault nor dropDefault');
+        }
+    }
+}
+
+
+
+
