313f91c93011403ae82ed88ff05d92d4af0e4908
c9s <yoanlin93@gmail.com>
2015-07-30 15:56:06 +0800

    Add cast support for PgSQLDriver

--- src/SQLBuilder/Driver/PgSQLDriver.php
+++ src/SQLBuilder/Driver/PgSQLDriver.php
@@ -9,5 +9,15 @@ class PgSQLDriver extends BaseDriver
     public function quoteIdentifier($id) {
         return '"' . addcslashes($id,'"') . '"';
     }
+
+    public function cast($value)
+    {
+        if ($value === true) {
+            return 1;
+        } else if ($value === false) {
+            return 0;
+        }
+        return $value;
+    }
 }
 
