8bec8133be04187914e3477bd9778ff3f8b62694
c9s <yoanlin93@gmail.com>
2017-01-20 11:16:49 +0800

    Remove ltrim

--- SQLBuilder/Universal/Syntax/Conditions.php
+++ SQLBuilder/Universal/Syntax/Conditions.php
@@ -235,7 +235,7 @@ class Conditions implements ToSqlInterface, Countable
                 $sql .= $driver->deflate($expr);
             }
         }
-        return ltrim($sql);
+        return $sql;
     }
 
     public function hasExprs()
