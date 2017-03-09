763c0112563c6c88828e6e6b93c2cd3b0aec4345
c9s <yoanlin93@gmail.com>
2017-03-10 00:26:44 +0800

    Add UUIDQuery

---
+++ src/Universal/Query/UUIDQuery.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Universal\Query;
+
+use Exception;
+use SQLBuilder\Exception\UnsupportedDriverException;
+use SQLBuilder\Driver\BaseDriver;
+use SQLBuilder\Driver\MySQLDriver;
+use SQLBuilder\ToSqlInterface;
+use SQLBuilder\ArgumentArray;
+use SQLBuilder\Universal\Syntax\Conditions;
+use SQLBuilder\Universal\Traits\OrderByTrait;
+use SQLBuilder\Universal\Traits\WhereTrait;
+use SQLBuilder\Universal\Traits\PagingTrait;
+use SQLBuilder\Universal\Expr\SelectExpr;
+use SQLBuilder\MySQL\Traits\PartitionTrait;
+use SQLBuilder\MySQL\Traits\IndexHintTrait;
+use SQLBuilder\Universal\Traits\JoinTrait;
+use SQLBuilder\Universal\Traits\OptionTrait;
+
+class UUIDQuery implements ToSqlInterface
+{
+    public function toSql(BaseDriver $driver, ArgumentArray $args)
+    {
+        if ($driver instanceof MySQLDriver) {
+            return 'SELECT UUID();';
+        }
+        if ($driver instanceof PgSQLDriver) {
+            return 'SELECT UUID_GENERATE_V4();';
+        }
+        if ($driver instanceof SQLiteDriver) {
+            return 'SELECT hex(randomblob(16));';
+        }
+        throw new UnsupportedDriverException($driver, $this);
+    }
+}
