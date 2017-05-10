58475b02d990409f7339310b8a2f710e6e26df92
c9s <yoanlin93@gmail.com>
2017-05-10 15:44:18 +0800

    Replace ArgumentArray implemention with ArrayObject

--- src/ArgumentArray.php
+++ src/ArgumentArray.php
@@ -2,41 +2,19 @@
 
 namespace SQLBuilder;
 
-use ArrayIterator;
-use ArrayAccess;
-use IteratorAggregate;
+use ArrayObject;
 
-class ArgumentArray implements ArrayAccess, IteratorAggregate
+class ArgumentArray extends ArrayObject
 {
-    /**
-     * @var array
-     *
-     *   {
-     *      :name => 'John',
-     *      :phone => 'Phone',
-     *   }
-     */
-    protected $args = array();
-
     /**
      * @var Bind[]
      */
     protected $bindings = array();
 
-    public function __construct(array $args = array())
-    {
-        $this->args = $args;
-    }
-
-    public function getIterator()
-    {
-        return new ArrayIterator($this->args);
-    }
-
     public function push(Bind $bind)
     {
         $this->bindings[] = $bind;
-        $this->args[$bind->getMarker()] = $bind->getValue();
+        $this[$bind->getMarker()] = $bind->getValue();
     }
 
     // Deprecated 
@@ -44,13 +22,13 @@ class ArgumentArray implements ArrayAccess, IteratorAggregate
     {
         // trigger_error("Bind::add is deprecated, please use Bind::bind instead.", E_USER_DEPRECATED);
         $this->bindings[] = $bind;
-        $this->args[$bind->getMarker()] = $bind->getValue();
+        $this[$bind->getMarker()] = $bind->getValue();
     }
 
     public function bind($bind)
     {
         $this->bindings[] = $bind;
-        $this->args[$bind->getMarker()] = $bind->getValue();
+        $this[$bind->getMarker()] = $bind->getValue();
     }
 
     public function getBindingByIndex($idx)
@@ -63,29 +41,9 @@ class ArgumentArray implements ArrayAccess, IteratorAggregate
         return $this->bindings;
     }
 
-    public function offsetSet($name, $value)
-    {
-        $this->args[$name] = $value;
-    }
-
-    public function offsetExists($name)
-    {
-        return isset($this->args[ $name ]);
-    }
-
-    public function offsetGet($name)
-    {
-        return $this->args[ $name ];
-    }
-
-    public function offsetUnset($name)
-    {
-        unset($this->args[$name]);
-    }
-
     public function getArgs()
     {
-        return $this->args;
+        return $this->getArrayCopy();
     }
 
     /**
@@ -99,13 +57,13 @@ class ArgumentArray implements ArrayAccess, IteratorAggregate
     {
         if ($removeBinds) {
             $args = array();
-            foreach ($this->args as $key => $val) {
+            foreach ($this as $key => $val) {
                 $args[$key] = $val instanceof Bind ? $val->getValue() : $val;
             }
 
             return $args;
         }
 
-        return $this->args;
+        return $this->getArrayCopy();
     }
 }
